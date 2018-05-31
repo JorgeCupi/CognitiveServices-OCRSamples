29d9d63d24a52f2ac7393dda99b6decb04dd1571
Jorge Cupi <jocupi@microsoft.com>
2018-05-31 12:10:09 -0500

    Sample transforms regions to rows of lines ordered by left and top margins

---
+++ code/WPF/Classes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF
+{
+    public class CognitiveResult
+    {
+        public string TextAngle { get; set; }
+        public string Orientation { get; set; }
+        public string Language { get; set; }
+        public List<Region> Regions { get; set; }
+    }
+    public class Region
+    {
+        public string BoundingBox { get; set; }
+        public List<Line> Lines { get; set; }
+    }
+
+    public class Line
+    {
+        public string BoundingBox { get; set; }
+        public List<Word> Words { get; set; }
+    }
+
+    public class Word
+    {
+        public string BoundingBox { get; set; }
+        public string Text { get; set; }
+
+        public int Left { get; set; }
+
+        public int Top { get; set; }
+    }
+}
